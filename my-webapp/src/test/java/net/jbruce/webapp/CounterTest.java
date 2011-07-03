43966eeffb27d149510cd826994da2612f3a6827
Jason Bruce <jbruce@jbruce.net>
2011-07-02 20:49:32 -0500

    Adding first Java code

---
+++ my-webapp/src/test/java/net/jbruce/webapp/CounterTest.java
@@ -0,0 +1,18 @@
+package net.jbruce.webapp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: SG0891064
+ * Date: Jul 2, 2011
+ * Time: 8:29:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CounterTest {
+    @Test
+    public void testInit() {
+        assertTrue(true);
+    }
+}
